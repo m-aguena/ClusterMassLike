18c3d4ed8e1f4b7921d19dc55fac61fe924ca766
Michel <aguena@lapp.in2p3.fr>
2026-08-19 12:22:55 +0200

    add simple powerlaw for wl and xray

--- cluster_mass_like/likelihoods/weak_lensing/wl_true_simple.py
+++ cluster_mass_like/likelihoods/weak_lensing/wl_true_simple.py
@@ -1,3 +1,10 @@
-def ln_prob_mwl_mtrue(mass_wl, mass_true, parameters):
+import numpy as np
+
+
+def ln_prob_mwl_mtrue(logm_wl, logm_true, parameters):
     """Log of the probability of having a WL mass given a true mass"""
-    pass
+    A, B, C = parameters
+    sig = C
+    return -0.5 * ((logm_wl - (A + B * logm_true)) / sig) ** 2 - 0.5 * np.log(
+        2 * np.pi * sig
+    )
--- cluster_mass_like/likelihoods/x_ray/xray_simple.py
+++ cluster_mass_like/likelihoods/x_ray/xray_simple.py
@@ -1,3 +1,10 @@
-def ln_prob_mxray_mtrue(mass_xray, mass_true, parameters):
+import numpy as np
+
+
+def ln_prob_mxray_mtrue(logm_xray, logm_true, parameters):
     """Log of the probability of having a x-ray mass given a true mass"""
-    pass
+    A, B, C = parameters
+    sig = C
+    return -0.5 * ((logm_xray - (A + B * logm_true)) / sig) ** 2 - 0.5 * np.log(
+        2 * np.pi * sig
+    )
