769dc8b53c227281afb3ce0e48ef689334ed6895
Michel <aguena@lapp.in2p3.fr>
2026-09-09 15:26:05 +0200

    add MagicMass.add_emass_table

--- magic_mass/mass.py
+++ magic_mass/mass.py
@@ -1,4 +1,5 @@
 import astropy.units as u
+from astropy.table import Table
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.constants import G
@@ -43,3 +44,44 @@ class MagicMass:
         M = self.radius**2 * integral
 
         return g, M
+
+    def add_emass_table(self, table, bootstrapped=False, outname="emass"):
+        out_table = Table()
+
+        if not bootstrapped:
+            out_table["magic_mass"] = self.measure_gt_mass(
+                theta=table["rp"],  # Mpc
+                # change units of ds from M_sol*Mpc**2 to M_sol*pc**2
+                DS=table["ds_t_pc2"],
+                # errs = table["ds_err_pc2"].T
+            )[1]
+            out_table["enclosed_mass_radius"] = (
+                np.ones(len(table))[:, None] * self.radius[None, :]
+            )
+
+        else:
+            out_table["magic_mass"] = np.array(
+                [
+                    self.measure_gt_mass(theta, DS, plot=False)[1]
+                    for theta, DS in zip(
+                        table["rp"].transpose(2, 0, 1),
+                        table["ds_t_pc2"].transpose(2, 0, 1),
+                    )
+                ]
+            ).transpose(1, 2, 0)
+
+            out_table["gt"] = np.array(
+                [
+                    self.measure_gt_mass(theta, DS, plot=False)[0]
+                    for theta, DS in zip(
+                        table["rp"].transpose(2, 0, 1),
+                        table["ds_t_pc2"].transpose(2, 0, 1),
+                    )
+                ]
+            ).transpose(1, 2, 0)
+
+            out_table["enclosed_mass_radius"] = (
+                np.ones(len(table))[:, None] * self.radius[None, :]
+            )
+
+        setattr(table, outname, out_table)
